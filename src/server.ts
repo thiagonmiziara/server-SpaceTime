446e71cb6fd28aef681576565381ec5996e47602
Thiago Miziara <thiago.miziara@luizalabs.com>
2023-05-18 09:16:16 -0300

    server nlw spacetime

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
+
+// npx prisma migrate dev --name  para criar uma migration
+// npx prisma studio para ver o banco de dados
